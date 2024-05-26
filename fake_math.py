54c0f4ca3ce794e62abd337246686e639afd9379
ElenaO <oea1982@mail.ru>
2024-05-26 19:56:17 +0500

    Initial commit

---
+++ fake_math.py
@@ -0,0 +1,11 @@
+def divide(first, second):
+
+    if second == 0:
+         result = "Ошибка"
+
+    else:
+        result = first / second
+
+    print(result)
+
+    return result
---
+++ main.py
@@ -0,0 +1,12 @@
+from fake_math import divide as fake_divide
+
+from true_math import divide as true_divide
+
+
+result_1 = fake_divide(69, 3)
+
+result_2 = fake_divide(3, 0)
+
+result_3 = true_divide(49, 7)
+
+result_4 = true_divide(15, 0)
